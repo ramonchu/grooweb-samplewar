c40dc35ceb7a26cf9d11b34de39eb5628e18d02b
ramon <ramon.arnau@gmail.com>
2013-03-14 01:37:51 +0100

    Adaptación de la aplicación de ejemplo con el uso de GrooAppModule

--- src/main/webapp/WEB-INF/groovy/conf/GrooModule.groovy
+++ src/main/webapp/WEB-INF/groovy/conf/GrooModule.groovy
@@ -1,8 +1,9 @@
 package conf;
 
-import com.google.inject.AbstractModule
+import com.arteco.grooweb.web.GrooAppModule
 
-public class GrooModule extends AbstractModule{
+
+public class GrooModule extends GrooAppModule{
 
 	@Override
 	protected void configure() {
